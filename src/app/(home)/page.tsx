b565ccb4145ed55666713cdeb9675d276060a7ad
tilo1306 <doug1306@gmail.com>
2024-11-28 13:40:50 -0300

    feat: ajust

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,24 +1,18 @@
-'use client'; // Marca o componente como Client-side
 
-import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 
-export default function Home() {
-  const searchParams = useSearchParams();
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
 
-  // Garantir que searchParams não seja nulo
-  const username =
-    (searchParams && decodeURIComponent(searchParams.get('userName') || 'Usuário não identificado')) || 'Usuário não identificado';
-
-  // Debug para verificar o objeto searchParams no console
-  console.log('searchParams:', searchParams?.toString());
-  console.log('username:', username);
-
+console.log(searchParams)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Suspense>
 
-      <h1>{`Bem-vindo, ${username}`}</h1>
+      <h1>{`Bem-vindo, ${decodeURIComponent( searchParams?.userName as string)}`}</h1>
       </Suspense>
     </div>
   );
