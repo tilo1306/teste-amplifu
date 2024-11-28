d95d2e87dd9121c49ab904e0f2b16746db640c89
tilo1306 <doug1306@gmail.com>
2024-11-28 13:46:03 -0300

    feat: a

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,13 +6,13 @@ export default async function Home({
 }: {
   searchParams?: { [key: string]: string | string[] | undefined };
 }) {
-
+const a = decodeURIComponent( searchParams?.userName as string)
 console.log(searchParams)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Suspense>
 
-      <h1>{`Bem-vindo, ${decodeURIComponent( searchParams?.userName as string)}`}</h1>
+      <h1>{`Bem-vindo, ${a}`}</h1>
       </Suspense>
     </div>
   );
