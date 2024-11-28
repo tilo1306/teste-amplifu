a2d45690c87e04bdafe0e689f4e963246a1671d6
tilo1306 <doug1306@gmail.com>
2024-11-28 13:31:08 -0300

    feat: ajust

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,43 +1,21 @@
-import { useEffect, useState } from 'react';
+'use client'; // Marca o componente como Client-side
+
+import { useSearchParams } from 'next/navigation';
 
 export default function Home() {
-  const [userName, setUserName] = useState('');
-  const [error, setError] = useState('');
+  const searchParams = useSearchParams();
 
-  useEffect(() => {
-    // Fazer uma requisição para a rota atual
-    const fetchUserInfo = async () => {
-      try {
-        const response = await fetch(window.location.href);
+  // Garantir que searchParams não seja nulo
+  const username =
+    (searchParams && decodeURIComponent(searchParams.get('userName') || 'Usuário não identificado')) || 'Usuário não identificado';
 
-        // Verifica se a resposta foi bem-sucedida
-        if (!response.ok) {
-          setError('Erro ao obter informações do usuário.');
-          return;
-        }
-
-        // Recupera o cabeçalho personalizado
-        const userInfo = response.headers.get('userInfo');
-        if (userInfo) {
-          const parsedInfo = JSON.parse(userInfo);
-          setUserName(parsedInfo.userName || 'Usuário não identificado');
-        }
-      } catch (err) {
-        setError('Erro ao buscar informações do usuário.');
-        console.error(err);
-      }
-    };
-
-    fetchUserInfo();
-  }, []);
+  // Debug para verificar o objeto searchParams no console
+  console.log('searchParams:', searchParams?.toString());
+  console.log('username:', username);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {error ? (
-        <h1 className="text-red-500">{error}</h1>
-      ) : (
-        <h1>{`Bem-vindo, ${userName}`}</h1>
-      )}
+      <h1>{`Bem-vindo, ${username}`}</h1>
     </div>
   );
 }
