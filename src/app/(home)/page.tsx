696278c09f012c0fab873a944c803bd2cab1f03d
tilo1306 <doug1306@gmail.com>
2024-11-28 13:34:30 -0300

    feat: teste

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 'use client'; // Marca o componente como Client-side
 
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 export default function Home() {
   const searchParams = useSearchParams();
@@ -15,7 +16,10 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Suspense>
+
       <h1>{`Bem-vindo, ${username}`}</h1>
+      </Suspense>
     </div>
   );
 }
