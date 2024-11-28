c64754cdd999254b61bd21c825d96fe9fcaac2f1
tilo1306 <doug1306@gmail.com>
2024-11-28 12:58:19 -0300

    feat: ajust

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,18 @@
-import { cookies } from 'next/headers';
+import { headers } from 'next/headers';
 
-export default function Home() {
-  // Acessar os cookies
-  const cookieStore = cookies();
-  const userName = cookieStore.get('userName')?.value || 'Usuário não identificado';
+export default async function Home() {
+  const headersList = headers();
+  const userInfo = headersList.get('userInfo');
+
+  let userName = 'Usuário não identificado';
+  if (userInfo) {
+    try {
+      const parsedInfo = JSON.parse(userInfo);
+      userName = decodeURIComponent(parsedInfo.userName) || userName;
+    } catch (error) {
+      console.error('Erro ao processar o cabeçalho userInfo:', error);
+    }
+  }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,44 +1,30 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  // Ignorar rotas específicas, como a página de erro
-  if (pathname === '/error') {
-    return NextResponse.next();
-  }
-
-  // Obter o parâmetro 'userName' da URL
+  
   const encodedUserName = request.nextUrl.searchParams.get('userName');
 
-  // Verificar se o parâmetro 'userName' existe
   if (!encodedUserName || encodedUserName.trim() === '') {
-    // Redirecionar para a página de erro, se não estiver presente
     return NextResponse.redirect(new URL('/error', request.url));
   }
 
-  // Decodificar o parâmetro 'userName'
+
   let userName;
   try {
-    userName = decodeURIComponent(encodedUserName);
+    userName = encodedUserName;
   } catch (error) {
     console.error('Erro ao decodificar o userName:', error);
     return NextResponse.redirect(new URL('/error', request.url));
   }
 
-  // Criar uma resposta e adicionar o cookie
+  
   const response = NextResponse.next();
-  response.cookies.set('userName', userName, {
-    httpOnly: true, // Evita acesso pelo JavaScript no cliente
-    secure: process.env.NODE_ENV === 'production', // Só envia cookies em conexões seguras no ambiente de produção
-    sameSite: 'strict', // Evita envio de cookies entre sites
-    path: '/', // Disponível em todo o site
-  });
+  response.headers.set('userInfo', JSON.stringify({ userName }));
 
   return response;
 }
 
-// Configuração para aplicar o middleware em todas as rotas
+
 export const config = {
-  matcher: '/:path*',
+  matcher: '/:path*', 
 };
