b3c61d68c9a160eb872dae3c44477a3ee18c5078
tilo1306 <doug1306@gmail.com>
2024-11-28 14:21:30 -0300

    feat: ajust

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,17 @@
+'use client'; // Este arquivo será renderizado no cliente
 
-import { Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | string[] | undefined };
-}) {
-const a = decodeURIComponent( searchParams?.userName as string)
-console.log(searchParams)
+export default function Home() {
+  // Usar o hook useSearchParams para acessar os parâmetros
+  const searchParams = useSearchParams();
+
+  // Recuperar o parâmetro 'userName'
+  const userName = decodeURIComponent(searchParams.get('userName') || 'Usuário não identificado');
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Suspense>
-
-      <h1>{`Bem-vindo, ${a}`}</h1>
-      </Suspense>
+      <h1>{`Bem-vindo, ${userName}`}</h1>
     </div>
   );
 }
